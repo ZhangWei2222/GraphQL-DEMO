2fd49eb0e32f64d56592c9410a1d793644485352
zhangwei2222 <985983437@qq.com>
2022-02-24 22:12:21 +0800

    demo: 简单例子

---
+++ hello.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const {
+  buildSchema
+} = require('graphql')
+const {
+  graphqlHTTP
+} = require('express-graphql')
+
+// 构建schema，这里定义查询的语句和类型
+const schema = buildSchema(`
+    type Query {
+        hero: String
+        name: String
+    }
+`)
+
+// 定义查询所对应的 resolver，也就是查询对应的处理器
+const root = {
+  hero: () => {
+    return "I'm iron man"
+  },
+  name: () => {
+    return '1'
+  }
+}
+
+const app = express()
+
+// 将路由转发给 graphqlHTTP 处理
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'))
